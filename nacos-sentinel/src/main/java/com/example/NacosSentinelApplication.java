2e9795c2fe7537c4214595221aef1deb57e22fcc
hehepeng <3239723884@qq.com>
2024-04-19 16:39:49 +0800

    feat: 将gateway-sentinel修改为nacos-sentinel

--- nacos-sentinel/src/main/java/com/example/NacosSentinelApplication.java
+++ nacos-sentinel/src/main/java/com/example/NacosSentinelApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewaySentinelApplication {
+public class NacosSentinelApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GatewaySentinelApplication.class, args);
+        SpringApplication.run(NacosSentinelApplication.class, args);
     }
 }
